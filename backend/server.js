3c89b7bc1ab26a78cdb38b8c96c4277014bc8739
Lydia Solomon <lydiasolomon97@gmail.com>
2022-10-16 15:43:04 +0300

    Get and create ticket.

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,7 @@ app.get("/", (req, res) => {
 
 // Routes
 app.use("/api/users", require("./routes/userRoute"));
+app.use("/api/tickets", require("./routes/ticketRoute"));
 
 app.use(errorHandler);
 
